69cce93efc519ddbfe26b4827f48d1a6fc6fa57b
rafael110307 <00001091007597sp@al.educacao.sp.gov.br>
2024-12-02 07:32:49 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Esportes',
+    'Qual o time com maior número de títulos da NBA?',
+    'O Boston Celtics, com 18 títulos.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Esportes',
+    'Qual é a única seleção bicampeã olímpica no volei?',
+    'A França, vencendo os jogos em 2021 e em 2024.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Natureza',
+    'Qual o maior animal do mundo?',
+    'A baleia azul, com cerca de 30 metros de comprimento.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Natureza',
+    'Qual o animal mais pesado do mundo terrestre?',
+    'O elefante-africano, podendo pesar em torno de 14 toneladas'
 )
